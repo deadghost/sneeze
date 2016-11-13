b8b11a6255ff5c5614087a7c6c4235e4d435b82b
deadghost <kennyoliu@gmail.com>
2016-11-14 03:52:26 +0800

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sneeze "0.1.0"
+(defproject sneeze "0.2.0"
   :description "Clojure HTML templating library. Hiccup + Selectors = Wow."
   :url "https://github.com/deadghost/sneeze"
   :license {:name "Eclipse Public License"
